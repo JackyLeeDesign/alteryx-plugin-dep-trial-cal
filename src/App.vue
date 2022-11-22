d013fe9e52349fd99b72f9fe054cc8517cc58412
PWCTW\JYKL <jackyleeidea@gmail.com>
2022-11-22 17:14:05 +0800

    feat: v0.2.6 調整輸出 Excel 欄位格式，填色與數值千分位

--- src/App.vue
+++ src/App.vue
@@ -180,7 +180,7 @@
                     不適用 (勾選以試算所有資料)
                   </b></label>
               </div>
-              <div v-if="!is_res_val_empty">
+              <div v-if="is_res_val_empty.toString()=='false'">
                 <label for="exampleFormControlInput2" class="form-label">
                   請選擇對應欄：
                 </label>
@@ -275,7 +275,7 @@
                   不需排除資產分類
                 </b></label>
             </div>
-            <div class="mb-3" v-if="!is_no_need_asset_filter">
+            <div class="mb-3" v-if="is_no_need_asset_filter.toString()=='false'">
               <div class="mb-2">
                 <div class="form-check form-check-inline">
                   <input class="form-check-input" type="radio" name="inlineDepDetermineRadioOptions"
@@ -288,7 +288,7 @@
                   <label class="form-check-label" for="inlineRadio2">按資產名稱進行排除</label>
                 </div>
               </div>
-              <div v-if="is_determine_num">
+              <div v-if="is_determine_num.toString()=='true'">
                 <!-- 資產分類(編號) -->
                 <label for="exampleFormControlInput1" class="form-label"><b>
                     <BIconColumns style="vertical-align:text-top;" class="icon" /> 資產分類(編號)
@@ -302,12 +302,12 @@
                   <option v-for="item, index in columns" v-bind:key="index">{{ item }}</option>
                 </select>
                 <label for="exampleFormControlInput1" class="form-check-label mb-2"><b>
-                    {{ is_determine_num ? "請輸入不計入折舊費用計算的資產編號抬頭代表號：" : "請輸入不計入折舊費用計算的資產名稱：" }}<br>( 若需排除多筆資料，可使用分號分隔 )
+                    {{ is_determine_num.toString()=='true' ? "請輸入不計入折舊費用計算的資產編號抬頭代表號：" : "請輸入不計入折舊費用計算的資產名稱：" }}<br>( 若需排除多筆資料，可使用分號分隔 )
                   </b></label>
                 <input type="text" id="determine_num1" class="form-control"
-                  :placeholder="is_determine_num ? '請輸入資產編號' : '請輸入資產名稱'" v-model="no_cal_keyword">
+                  :placeholder="is_determine_num.toString()=='true' ? '請輸入資產編號' : '請輸入資產名稱'" v-model="no_cal_keyword">
               </div>
-              <div class="mb-3" v-if="!is_determine_num">
+              <div class="mb-3" v-if="is_determine_num.toString()=='false'">
                 <!-- 資產分類(名稱) -->
                 <label for="exampleFormControlInput1" class="form-label"><b>
                     <BIconColumns style="vertical-align:text-top;" class="icon" /> 資產分類(名稱)
@@ -321,10 +321,10 @@
                   <option v-for="item, index in columns" v-bind:key="index">{{ item }}</option>
                 </select>
                 <label for="exampleFormControlInput1" class="form-check-label"><b>
-                    {{ is_determine_num ? "請輸入不計入折舊費用計算的資產編號抬頭代表號：" : "請輸入不計入折舊費用計算的資產名稱：" }}<br>( 若需排除多筆資料，可使用分號分隔 )
+                    {{ is_determine_num.toString()=='true' ? "請輸入不計入折舊費用計算的資產編號抬頭代表號：" : "請輸入不計入折舊費用計算的資產名稱：" }}<br>( 若需排除多筆資料，可使用分號分隔 )
                   </b></label>
                 <input type="text" id="determine_num1" class="form-control"
-                  :placeholder="is_determine_num ? '請輸入資產編號' : '請輸入資產名稱'" v-model="no_cal_keyword">
+                  :placeholder="is_determine_num.toString()=='true' ? '請輸入資產編號' : '請輸入資產名稱'" v-model="no_cal_keyword">
               </div>
             </div>
           </div>
@@ -337,7 +337,7 @@
   </div>
 
   <footer class="footer mt-auto">
-    <p class="text-muted" style="margin: 0px;text-align: center;">版本：0.2.4</p>
+    <p class="text-muted" style="margin: 0px;text-align: center;">版本：0.2.6</p>
   </footer>
 
 </template>
@@ -377,17 +377,17 @@ export default {
       assets_name_col: "", // 資產分類名稱
       asset_cost_col: "", // 固定資產成本
       durable_period_col: "", // 耐用年限
-      is_durable_period_year: true, //耐用年限是否為年份
+      is_durable_period_year: "", //耐用年限是否為年份
       reserved_residual_values_col: "", // 預留殘值
       dep_start_date_col: "", // 折舊起始日期
       amount_of_this_period_col: "", // 本期提列數
       no_cal_keyword: "", // 欲排除之會科關鍵字
-      dep_trial_way: false, // 折舊起算方式, 0=>15天為期, 1=>下個月計算
+      dep_trial_way: "", // 折舊起算方式, 0=>15天為期, 1=>下個月計算
       input_isConnectFile: "",
-      is_determine_num: true, // 是否依會科判斷
+      is_determine_num: "", // 是否依會科判斷
       save_dir: "", // 儲存路徑
-      is_no_need_asset_filter: false, // 不需排除會科
-      is_res_val_empty: false, // 無預留殘值
+      is_no_need_asset_filter: "", // 不需排除會科
+      is_res_val_empty: "", // 無預留殘值
       quarter_month: "", // 季度選項
 
       // 畫面控制相關參數
@@ -571,20 +571,20 @@ export default {
           manager.addDataItem(dep_start_date_col)
           var amount_of_this_period_col = new AlteryxDataItems.SimpleString('amount_of_this_period_col')
           manager.addDataItem(amount_of_this_period_col)
-          var is_durable_period_year = new AlteryxDataItems.SimpleBool('is_durable_period_year')
+          var is_durable_period_year = new AlteryxDataItems.SimpleString('is_durable_period_year')
           manager.addDataItem(is_durable_period_year)
           var no_cal_keyword = new AlteryxDataItems.SimpleString('no_cal_keyword')
           manager.addDataItem(no_cal_keyword)
-          var is_determine_num = new AlteryxDataItems.SimpleBool('is_determine_num')
+          var is_determine_num = new AlteryxDataItems.SimpleString('is_determine_num')
           manager.addDataItem(is_determine_num)
-          var dep_trial_way = new AlteryxDataItems.SimpleBool('dep_trial_way')
+          var dep_trial_way = new AlteryxDataItems.SimpleString('dep_trial_way')
           manager.addDataItem(dep_trial_way)
           var save_dir = new AlteryxDataItems.SimpleString('save_dir')
           manager.addDataItem(save_dir)
           manager.bindDataItemToWidget(save_dir, 'save_dir')
-          var is_no_need_asset_filter = new AlteryxDataItems.SimpleBool('is_no_need_asset_filter')
+          var is_no_need_asset_filter = new AlteryxDataItems.SimpleString('is_no_need_asset_filter')
           manager.addDataItem(is_no_need_asset_filter)
-          var is_res_val_empty = new AlteryxDataItems.SimpleBool('is_res_val_empty')
+          var is_res_val_empty = new AlteryxDataItems.SimpleString('is_res_val_empty')
           manager.addDataItem(is_res_val_empty)
           var quarter_month = new AlteryxDataItems.SimpleString('quarter_month')
           manager.addDataItem(quarter_month)
@@ -621,7 +621,7 @@ export default {
             this.is_determine_num = true
           }
           if (this.is_durable_period_year === "") {
-            this.is_durable_period_year = false
+            this.is_durable_period_year = true
           }
           if (this.dep_trial_way === "") {
             this.dep_trial_way = false
